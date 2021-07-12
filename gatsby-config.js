d10d4ea4748277246e8b509b6b5386f76e9bd9e9
Rodrigo Moreira <hola@piducan.dev>
2021-07-12 13:44:18 -0400

    add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `maulina`,
+    description: `comunidad de fotografía.`,
+    author: `@marcelavaldes`,
+    siteUrl: `https://maulina.cl/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Maulina`,
+        short_name: `Maulina`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
