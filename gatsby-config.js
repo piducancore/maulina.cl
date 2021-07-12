c65ca7a1039d8ba33bb36099072b6d2e0bdd1510
Rodrigo Moreira <hola@piducan.dev>
2021-07-12 12:28:42 -0400

    add theme-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,8 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-theme-ui`,
+    // `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
