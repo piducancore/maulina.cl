b3525c7856087b73b014c41a4faa0cb0cc690870
Rodrigo Moreira <hola@piducan.dev>
2021-07-14 15:05:14 -0400

    create menu component

---
+++ src/components/menu.js
@@ -0,0 +1,26 @@
+/** @jsx jsx */
+import { jsx, Themed } from "theme-ui"
+import { Link } from "gatsby"
+import slugify from "slugify"
+
+function MenuItem({ children }) {
+  return (
+    <Themed.p sx={{ fontSize: 0, my: 0 }}>
+      <Themed.a as={Link} to={"/" + slugify(children).toLowerCase()}>
+        {children}
+      </Themed.a>
+    </Themed.p>
+  )
+}
+
+export default function Menu({ ...props }) {
+  return (
+    <div {...props} sx={{ textAlign: "right" }}>
+      <MenuItem>Somos</MenuItem>
+      <MenuItem>Fotógrafas</MenuItem>
+      <MenuItem>Galería</MenuItem>
+      <MenuItem>Participa</MenuItem>
+      <MenuItem>Contacto</MenuItem>
+    </div>
+  )
+}
