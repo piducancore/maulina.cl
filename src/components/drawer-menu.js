73e4aaff7cf934df5ffa90598ef5710cd18a632f
Rodrigo Moreira <hola@piducan.dev>
2021-07-20 11:47:00 -0400

    create drawer

---
+++ src/components/drawer-menu.js
@@ -0,0 +1,41 @@
+/** @jsx jsx */
+import { jsx, Themed, Container } from "theme-ui"
+import { Link } from "gatsby"
+import slugify from "slugify"
+import { alpha } from "@theme-ui/color"
+
+export const items = ["Somos", "Fotógrafas", "Galería", "Blog", "Contacto"]
+
+export default function DrawerMenu({ isOpen, close }) {
+  return (
+    <div
+      sx={{
+        position: "absolute",
+        height: "100%",
+        width: "100%",
+        left: isOpen ? "0%" : "-100%",
+        bg: alpha("secondary", 0.96),
+        // display: "flex",
+        // flexDirection: "column",
+        // justifyContent: "center",
+        zIndex: 99,
+        opacity: isOpen ? 1 : 0,
+      }}
+    >
+      <Container sx={{ my: 6 }}>
+        {items.map(item => (
+          <Themed.p key={item}>
+            <Themed.a
+              as={Link}
+              to={"/" + slugify(item).toLowerCase()}
+              sx={{ my: 0, color: "background" }}
+              onClick={close}
+            >
+              {item}
+            </Themed.a>
+          </Themed.p>
+        ))}
+      </Container>
+    </div>
+  )
+}
