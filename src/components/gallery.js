cfaf190039cbdd34a61a37cb75af7b9a76a21504
Rodrigo Moreira <hola@piducan.dev>
2021-07-29 11:27:22 -0400

    receive props

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,14 +1,14 @@
 /** @jsx jsx */
 import { jsx, Flex } from "theme-ui"
-import React from "react"
+// import React from "react"
 
-export default function GalleryFlex({ children, size }) {
+export default function GalleryFlex({ children, size, ...props }) {
   let rows = []
   for (let i = 0; i < children.length; i += size) {
     rows.push(children.slice(i, i + size))
   }
   return (
-    <React.Fragment>
+    <div {...props}>
       {rows.map((elements, index) => {
         return (
           <Flex key={index} sx={{ flexDirection: ["column", "row"] }}>
@@ -16,6 +16,6 @@ export default function GalleryFlex({ children, size }) {
           </Flex>
         )
       })}
-    </React.Fragment>
+    </div>
   )
 }
