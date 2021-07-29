08a845f26a18b0097eb1c0dce877ca98db68755a
Rodrigo Moreira <hola@piducan.dev>
2021-07-29 12:23:01 -0400

    use gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,67 @@
 /** @jsx jsx */
-import { jsx, Flex } from "theme-ui"
-import { StaticImage } from "gatsby-plugin-image"
+import { jsx, Themed } from "theme-ui"
+import { graphql, Link } from "gatsby"
+import shuffle from "array-shuffle"
 
 import Layout from "../components/layout"
+import Gallery from "../components/gallery"
+import Image from "../components/image"
 
-const IndexPage = () => (
-  <Layout seo={{ title: "Pronto..." }}>
-    <Flex sx={{ height: 540 }}>
-      <StaticImage
-        src="../images/maulina.png"
-        width={300}
-        placeholder="blurred"
-        quality={95}
-        formats={["AUTO", "WEBP", "AVIF"]}
-        alt="Maulina"
-        sx={{ m: "auto" }}
-      />
-    </Flex>
-  </Layout>
-)
+export default function IndexPage({ data }) {
+  const allPhotos = data.allContentfulArtista.nodes
+    .map(({ username, featured, gallery }) =>
+      [featured].concat(gallery).map(photo => {
+        photo.username = username
+        return photo
+      })
+    )
+    .flat()
+  const shuffled = shuffle(allPhotos)
+  return (
+    <Layout seo={{ title: "Inicio" }}>
+      <Gallery size={3} sx={{ flexDirection: ["column", "row"] }}>
+        {shuffled.map(photo => {
+          console.log(photo)
+          return (
+            <Themed.a key={photo.title} as={Link} to={"/" + photo.username}>
+              <Image
+                data={photo}
+                key={photo.title}
+                alt={photo.title}
+                overlay={photo.title}
+              />
+            </Themed.a>
+          )
+        })}
+      </Gallery>
+    </Layout>
+  )
+}
 
-export default IndexPage
+export const query = graphql`
+  query IndexQuery {
+    allContentfulArtista {
+      nodes {
+        username
+        featured {
+          contentful_id
+          title
+          gatsbyImageData(
+            height: 480
+            quality: 100
+            placeholder: DOMINANT_COLOR
+          )
+        }
+        gallery {
+          contentful_id
+          title
+          gatsbyImageData(
+            height: 480
+            quality: 100
+            placeholder: DOMINANT_COLOR
+          )
+        }
+      }
+    }
+  }
+`
