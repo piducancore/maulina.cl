dbfc2a8e036940f49f84e6eb46e52ccc831ed457
Rodrigo Moreira <hola@piducan.dev>
2021-08-02 22:30:14 -0400

    manifest colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,8 @@ module.exports = {
         name: `Maulina`,
         short_name: `Maulina`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
         icon: `src/images/maulina-icon.png`, // This path is relative to the root of the site.
       },
