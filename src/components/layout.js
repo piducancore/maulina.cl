4f1b3eba2e594f6643364039c297e11dc6671f2d
Rodrigo Moreira <hola@piducan.dev>
2021-08-05 00:00:40 -0400

    simplify

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,16 @@
 /** @jsx jsx */
-import { jsx, Themed, Container, Grid, Box, Flex } from "theme-ui"
+import { jsx, Container } from "theme-ui"
 import React, { useEffect } from "react"
 
 import Seo from "./seo"
 import Header from "./header"
 import Footer from "./footer"
-import MenuIcon from "./menu-icon"
-import Logo from "./logo"
 import DrawerMenu from "./drawer-menu"
 
-import useStore from "../state"
-
-const Layout = ({ children, location, pageContext, seo }) => {
-  const { toggleMenu } = useStore()
+const Layout = ({ children, /* location, */ pageContext, seo }) => {
   // check if current page is root
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isHome = location?.pathname === rootPath
+  // const rootPath = `${__PATH_PREFIX__}/`
+  // const isHome = location?.pathname === rootPath
 
   // disable context menu
   useEffect(() => {
@@ -32,47 +27,11 @@ const Layout = ({ children, location, pageContext, seo }) => {
         image={seo?.image}
       />
       <DrawerMenu />
-      <div sx={{ variant: "layout.root" }}>
-        {
-          /* isHome ? (
-          <React.Fragment>
-            <Grid columns={[1, "1fr 3fr", "1fr 4fr"]} gap={0}>
-              <Box>
-                <Flex
-                  sx={{
-                    flexDirection: ["row", "column"],
-                    position: "sticky",
-                    top: 0,
-                    alignSelf: "start",
-                  }}
-                >
-                  <MenuIcon
-                    onClick={toggleMenu}
-                    sx={{ cursor: "pointer", mr: "auto" }}
-                  />
-                  <div sx={{ display: "flex", flexDirection: "column" }}>
-                    <Logo height={"100%"} sx={{ mx: 4, height: [32, 88] }} />
-                    <Themed.p sx={{ display: ["none", "inherit"] }}>
-                      Comunidad de fotografía
-                    </Themed.p>
-                  </div>
-                  <div></div>
-                </Flex>
-              </Box>
-              <Box>
-                <main sx={{ variant: "layout.main.cover" }}>{children}</main>
-              </Box>
-            </Grid>
-          </React.Fragment>
-        ) : */ <React.Fragment>
-            <Header />
-            <main sx={{ variant: "layout.main" }}>
-              <Container>{children}</Container>
-            </main>
-          </React.Fragment>
-        }
-        <Footer />
-      </div>
+      <Header />
+      <main sx={{ variant: "layout.main" }}>
+        <Container>{children}</Container>
+      </main>
+      <Footer />
     </React.Fragment>
   )
 }
